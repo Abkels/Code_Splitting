e603ce56ad28878a5fb25903cd82b3c217a651d7
Abkels <abrahamk29@gmail.com>
2023-10-18 04:05:22 +0100

    using funtional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,8 @@ import './App.css';
 
 
 import Page1 from './Components/Page1';
-// Part 1 - No Code Splitting
-// import Page2 from './Components/Page2';
-// import Page3 from './Components/Page3';
-// Part 3 - Cleaner Code Splitting
-// import AsyncComponent from './AsyncComponent';
 
-// part 4 - React.lazy
+// Dynamic import
 const Page2Lazy = React.lazy(()=> import('../src/Components/Page2'))
 const Page3Lazy = React.lazy(()=> import('../src/Components/Page3'))
 
@@ -18,63 +13,13 @@ class App extends Component {
     super();
     this.state = {
       route: 'page1',
-      // Part 2 - Code Splitting - manual
-      // component: null
     }
   }
   onRouteChange = (route) => {
-    // Part 1 - No Code Splitting
     this.setState({ route: route });
-    // Part 2 - Code Splitting - manual
-    // if (route === 'page1') {
-    //   this.setState({ route: route })
-    // } else if (route === 'page2') {
-    //   import('./Components/Page2')
-    //     .then((Page2) => {
-    //       this.setState({ route: route, component: Page2.default })
-    //     })
-    //     .catch(err => {
-    //     });
-    // } else {
-    //   import('./Components/Page3')
-    //     .then((Page3) => {
-    //       this.setState({ route: route, component: Page3.default })
-    //     })
-    //     .catch(err => {
-    //       console.log(err)
-    //     });
-    // }
   }
   
   render() {
-    // Part 1 - No code splitting
-    // if (this.state.route === 'page1') {
-    //   return <Page1 onRouteChange={this.onRouteChange} />
-    // } else if (this.state.route === 'page2') {
-    //   return <Page2 onRouteChange={this.onRouteChange} />
-    // } else {
-    //   return <Page3 onRouteChange={this.onRouteChange} />
-    // }
-
-    // Part 2 - No Code Splitting - manual
-    // if (this.state.route === 'page1') {
-    //   return <Page1 onRouteChange={this.onRouteChange} />
-    // } else {
-    //   return <this.state.component onRouteChange={this.onRouteChange} />
-    // }
-
-    // Part 3 - Cleaner Code Splitting
-    // if (this.state.route === 'page1') {
-    //   return <Page1 onRouteChange={this.onRouteChange} />
-    // } else if (this.state.route === 'page2') {
-    //   const AsyncPage2 = AsyncComponent(() => import("./Components/Page2"));
-    //   return <AsyncPage2 onRouteChange={this.onRouteChange} />
-    // } else {
-    //   const AsyncPage3 = AsyncComponent(() => import("./Components/Page3"));
-    //   return <AsyncPage3 onRouteChange={this.onRouteChange} />
-    // }
-    
-    // Part 4 - React.Lazy
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />
     } else if (this.state.route === 'page2') {
@@ -90,9 +35,97 @@ class App extends Component {
           <Page3Lazy onRouteChange={this.onRouteChange} />
         </Suspense>
       )
-    } 
+    }
      
   }
 }
 
 export default App
+
+// Using functional components
+// import React, { useState, Suspense } from 'react';
+// import './App.css';
+// import logo from './logo.svg';
+
+// import Page1 from './Components/Page1';
+
+// const Page2Lazy = React.lazy(() => import('./Components/Page2'));
+// const Page3Lazy = React.lazy(() => import('./Components/Page3'));
+
+// const App = () => {
+//   const [route, setRoute] = useState('page1');
+
+//   const onRouteChange = (newRoute) => {
+//     setRoute(newRoute);
+//   }
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1 className="App-title">Welcome to {route === 'page1' ? 'Page 1' : route === 'page2' ? 'Page 2' : 'Page 3'}</h1>
+//       </header>
+
+//       {route === 'page1' ? (
+//         <Page1 onRouteChange={onRouteChange} />
+//       ) : route === 'page2' ? (
+//         <Suspense fallback={<div>Loading...</div>}>
+//           <Page2Lazy onRouteChange={onRouteChange} />
+//         </Suspense>
+//       ) : (
+//         <Suspense fallback={<div>Loading...</div>}>
+//           <Page3Lazy onRouteChange={onRouteChange} />
+//         </Suspense>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// /* using react-router-dom is a better option
+// 1.Organize ur project structure with components 
+// src/
+//   components/
+//     Page1.js
+//     Page2.js
+//     ...
+//     Page10.js
+//   App.js
+
+//   2. use BrowserRouter and link in your app.js
+//   import React from 'react';
+// import { BrowserRouter, Route, Link } from 'react-router-dom';
+// import './App.css';
+// import logo from './logo.svg';
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to Your App</h1>
+//         </header>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/page1">Page 1</Link>
+//             </li>
+//             <li>
+//               <Link to="/page2">Page 2</Link>
+//             </li>
+//             {/* Add similar Link components for Page3 to Page10 */}
+//             </ul>
+//             </nav>
+//             <Route path="/page1" component={Page1} />
+//             <Route path="/page2" component={Page2} />
+//             {/* Add similar Route components for Page3 to Page10 */}
+//           </div>
+//         </BrowserRouter>
+//       );
+//     }
+    
+//     export default App;
+    
